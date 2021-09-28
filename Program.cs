b528fa3ae234d3b218ffa7a105055b8d4c7406db
hotbagels <81516937+hotbagels@users.noreply.github.com>
2021-09-28 16:16:42 -0400

    added last name, id, and photo url propertieswith instance methods for each, as well as a template for formating PrintEmployee

--- Program.cs
+++ Program.cs
@@ -6,35 +6,44 @@ namespace CatWorx.BadgeMaker
     class Program
     {
 
-        static List<string> GetEmployees()
+        static List<Employee> GetEmployees()
         {
             // return a list of strings
-            List<string> employees = new List<string>();
+            List<Employee> employees = new List<Employee>();
             // Collect user values until the value is an empty string
             while (true)
             {
-                Console.WriteLine("Please enter a name: (leave empty to exit): ");
-                string input = Console.ReadLine();
+                Console.WriteLine("Enter first name (leave empty to exit): ");
+                string firstName = Console.ReadLine();
                 // Break if the user hits ENTER without typing a name
-                if (input == "")
+                if (firstName == "")
                 {
                     break;
                 }
-                Employee currentEmployee = new Employee(input, "Smith");
-                employees.Add(currentEmployee.GetName());
+                Console.WriteLine("Enter a Last Name: ");
+                string lastName = Console.ReadLine();
+
+                Console.WriteLine("Enter ID: ");
+                int id = Int32.Parse(Console.ReadLine());
+                Console.Write("Enter Photo URL:");
+                // https://placekitten.com/300/300
+                string photoUrl = Console.ReadLine();
+                Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
+                employees.Add(currentEmployee);
             }
             return employees;
         }
-        static void PrintEmployees(List<string> employees)
+        static void PrintEmployees(List<Employee> employees)
         {
             for (int i = 0; i < employees.Count; i++)
             {
-                Console.WriteLine(employees[i]);
+                string template = "{0,-10}\t{1,-20}\t{2}";
+                Console.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetName(), employees[i].GetPhotoUrl()));
             }
         }
         static void Main(string[] args)
         {
-            List<string> employees = GetEmployees();
+            List<Employee> employees = GetEmployees();
             PrintEmployees(employees);
         }
     }
