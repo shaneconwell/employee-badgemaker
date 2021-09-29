6b0f23fff82c5baacec74a5a29df601c8489c16c
hotbagels <81516937+hotbagels@users.noreply.github.com>
2021-09-29 11:51:54 -0400

    Started building the MakeBadge method in Util. Added template badge, using webclient to to append web image on badge

--- Program.cs
+++ Program.cs
@@ -33,20 +33,13 @@ namespace CatWorx.BadgeMaker
             }
             return employees;
         }
-        // static void PrintEmployees(List<Employee> employees)
-        // {
-        //     for (int i = 0; i < employees.Count; i++)
-        //     {
-        //         string template = "{0,-10}\t{1,-20}\t{2}";
-        //         Console.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetName(), employees[i].GetPhotoUrl()));
-        //     }
-        // }
         static void Main(string[] args)
         {
             List<Employee> employees = GetEmployees();
             Util.PrintEmployees(employees);
             // run CSV creation method
             Util.MakeCSV(employees);
+            Util.MakeBadges(employees);
         }
     }
 }
--- Util.cs
+++ Util.cs
@@ -1,6 +1,9 @@
 using System;
 using System.IO;
+using System.Net;
+using System.Drawing;
 using System.Collections.Generic;
+
 namespace CatWorx.BadgeMaker
 {
     class Util
@@ -35,6 +38,47 @@ namespace CatWorx.BadgeMaker
                 }
 
             }
+        }
+        public static void MakeBadges(List<Employee> employees)
+        {
+            // Layout variables
+            int BADGE_WIDTH = 669;
+            int BADGE_HEIGHT = 1044;
+            int COMPANY_NAME_START_X = 0;
+            int COMPANY_NAME_START_Y = 110;
+            int COMPANY_NAME_WIDTH = 100;
+            int PHOTO_START_X = 184;
+            int PHOTO_START_Y = 215;
+            int PHOTO_WIDTH = 302;
+            int PHOTO_HEIGHT = 302;
+            int EMPLOYEE_NAME_START_X = 0;
+            int EMPLOYEE_NAME_START_Y = 560;
+            int EMPLOYEE_NAME_WIDTH = BADGE_WIDTH;
+            int EMPLOYEE_NAME_HEIGHT = 100;
+            int EMPLOYEE_ID_START_X = 0;
+            int EMPLOYEE_ID_START_Y = 690;
+            int EMPLOYEE_ID_WIDTH = BADGE_WIDTH;
+            int EMPLOYEE_ID_HEIGHT = 100;
+
+            // instance of WebClient is disposed after code in the block has run
+            using (WebClient client = new WebClient())
+            {
+                for (int i = 0; i < employees.Count; i++)
+                {
+
+                    Image photo = Image.FromStream(client.OpenRead(employees[i].GetPhotoUrl()));
+                    Image background = Image.FromFile("badge.png");
+                    // background.Save("data/employeeBadge.png");
+                    Image badge = new Bitmap(BADGE_WIDTH, BADGE_HEIGHT);
+                    Graphics graphic = Graphics.FromImage(badge);
+                    graphic.DrawImage(background, new Rectangle(0, 0, BADGE_WIDTH, BADGE_HEIGHT));
+                    graphic.DrawImage(photo, new Rectangle( PHOTO_START_X, PHOTO_START_Y, PHOTO_WIDTH, PHOTO_HEIGHT));
+                    badge.Save("data/employeeBadge.png");
+
+                }
+            }
+            // Create image.
+
         }
     }
 }
